c842af8d921397243ec225d8bfdfa9f002ce0203
Charles Moyes <cmoyes@jumptrading.com>
2025-10-10 14:49:24 +0000

    Export envrionment variable, add link

--- build.rs
+++ build.rs
@@ -1,27 +1,31 @@
 use std::{env, fs, path::PathBuf};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    println!("cargo:rerun-if-changed=proto/");
-
     let proto_dir = PathBuf::from("proto");
-    let mut proto_files = vec![];
+    let abs = env::current_dir()
+        .expect("cwd")
+        .join(&proto_dir)
+        .canonicalize()
+        .expect("canonicalize proto dir");
 
-    // Build all .proto files
-    for entry in fs::read_dir(&proto_dir)? {
+    println!("cargo:rerun-if-changed={}", abs.display());
+    println!("cargo:PROTO_DIR={}", abs.display());
+
+    let mut proto_files = vec![];
+    for entry in fs::read_dir(&abs)? {
         let path = entry?.path();
         if path.extension().and_then(|e| e.to_str()) == Some("proto") {
+            println!("cargo:rerun-if-changed={}", path.display());
             proto_files.push(path);
         }
     }
 
     let out_dir = PathBuf::from(env::var("OUT_DIR")?);
-
     let mut config = prost_build::Config::new();
-
     config.out_dir(&out_dir);
     config.compile_protos(
         &proto_files.iter().map(|p| p.display().to_string()).collect::<Vec<_>>(),
-        &["proto"],
+        &[abs],
     )?;
 
     Ok(())
